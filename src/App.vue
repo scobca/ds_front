8a586a2283f90ed9161d63e017165ae72e1ada46
scobca <scobca18@yandex.ru>
2023-08-12 10:06:59 +0300

    build app

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,21 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
   <router-view/>
+    <p>fdlkj</p>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script lang="ts">
 
-nav {
-  padding: 30px;
-}
+import {Options, Vue} from "vue-class-component";
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+@Options({
+    name: 'App',
+    components: {},
+})
+export default class App extends Vue {
 
-nav a.router-link-exact-active {
-  color: #42b983;
 }
+</script>
+
+<style scoped>
+
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,26 @@
+//application
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from "@/App.vue";
 import router from './router'
-import store from './store'
+import store from "@/store/store";
 
-createApp(App).use(store).use(router).mount('#app')
+//vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+//animate.css library
+import 'animate.css'
+
+//styles
+// import '@/assets/styles/global_classes.css'
+import '@/assets/css/main.css'
+// import '@/assets/styles/colors.css'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
+createApp(App).use(vuetify).use(store).use(router).mount('#app')
