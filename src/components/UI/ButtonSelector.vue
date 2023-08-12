d01445bdc1e5008d6002010849dc301dba1a695e
scobca <scobca18@yandex.ru>
2023-08-12 11:08:42 +0300

    ButtonSelector.vue

---
+++ src/components/UI/ButtonSelector.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="container">
+        <v-btn-toggle v-model="toggle" class="btn_container" color="primary" variant="outlined">
+            <v-btn class="button"> 1 </v-btn>
+            <v-btn class="button"> 2 </v-btn>
+            <v-btn class="button"> 3 </v-btn>
+            <v-btn class="button"> 4 </v-btn>
+        </v-btn-toggle>
+    </div>
+</template>
+
+<script lang="ts">
+
+import {Options, Vue} from "vue-class-component";
+
+@Options({
+    name: 'ButtonSelector',
+})
+export default class ButtonSelector extends Vue {
+    toggle: boolean | null = null
+}
+</script>
+
+<style scoped>
+
+.btn_container {
+    background-color: var(--container-bg-color) !important;
+    display: grid;
+    grid-template-columns: min-content min-content;
+    width: 12rem;
+}
+
+.button {
+    width: 6rem;
+}
+</style>
