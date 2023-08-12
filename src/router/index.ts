d9209dadfa1fd09aaa586ed6ee9eb9a9abf254fa
scobca <scobca18@yandex.ru>
2023-08-12 12:42:42 +0300

    new routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,16 +1,26 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import {createRouter, createWebHistory, RouteRecordRaw} from 'vue-router'
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    name: 'home',
-    component: () => import('@/views/HomeView.vue')
-  },
+    {
+        path: '/home/first',
+        name: 'FHome',
+        component: () => import('@/views/HomeView.vue')
+    },
+    {
+        path: '/home/second',
+        name: 'SHome',
+        component: () => import('@/views/HomeView.vue')
+    },
+    {
+        path: '/home/third',
+        name: 'THome',
+        component: () => import('@/views/HomeView.vue')
+    },
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes
 })
 
 export default router
