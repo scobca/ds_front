09b9195c2f21b525a24b3634d2b4b7a7b68f8f39
scobca <scobca18@yandex.ru>
2023-08-12 10:40:36 +0300

    added CoolButton.vue and LeftBar.vue

---
+++ src/components/UI/CoolButton.vue
@@ -0,0 +1,31 @@
+<template>
+    <button class="btn">
+        <slot></slot>
+    </button>
+</template>
+
+<script lang="ts">
+
+import {Options, Vue} from "vue-class-component";
+import {Prop} from "vue-property-decorator";
+
+@Options({
+    name: 'CoolButton'
+})
+export default class CoolButton extends Vue {
+
+}
+</script>
+
+<style scoped>
+.btn {
+    background: linear-gradient(200.42deg, #B275FF 13.57%, #C247ED 98.35%);
+    box-shadow: none;
+    border: none;
+    border-radius: 0.5rem;
+    color: var(--text-color);
+    cursor: pointer;
+    padding: 0.5rem;
+    max-height: fit-content;
+}
+</style>
---
+++ src/components/UI/LeftBar.vue
@@ -0,0 +1,85 @@
+<template>
+    <div class="main">
+        <h1 class="header">Меню</h1>
+        <div class="buttons_container">
+            <router-link to="/" class="button">
+                <div class="block">
+                    <p> Главная </p>
+                </div>
+            </router-link>
+
+            <router-link to="/client/registration" class="button">
+                <div class="block">
+                    <p> Кнопка </p>
+                </div>
+            </router-link>
+            <router-link to="" class="button">
+                <div class="block">
+                    <p> Кнопка </p>
+                </div>
+            </router-link>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import {Options, Vue} from "vue-class-component";
+
+@Options({
+    name: 'LeftBar',
+    components: {}
+})
+export default class LeftBar extends Vue {
+
+}
+</script>
+
+<style scoped>
+.main {
+    position: sticky;
+    height: 100%;
+    max-height: 13rem;
+    min-width: 10rem;
+    border-radius: 0 1rem 1rem 0;
+    background-color: var(--container-bg-color);
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+}
+
+.buttons_container {
+    display: flex;
+    flex-direction: column;
+    margin: 0;
+    align-items: flex-start;
+}
+
+.button {
+    text-decoration: none;
+    border-bottom: 1px solid var(--border-menu-color);
+    height: 2.25rem;
+    min-width: inherit;
+    box-sizing: border-box;
+
+    display: flex;
+    text-indent: .5rem;
+    justify-content: flex-start;
+    align-items: center;
+}
+
+.button:hover .img {
+    transform: scale(1.2);
+}
+
+.block {
+    display: flex;
+    align-items: center;
+}
+
+.img {
+    height: 1.1rem;
+    margin-left: .5rem;
+    transform: scale(1);
+    transition: 500ms;
+}
+</style>
