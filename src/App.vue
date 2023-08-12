bcd9c6f70d23d5a66746b6d726b878cd2a3ef4e8
scobca <scobca18@yandex.ru>
2023-08-12 10:40:47 +0300

    assets

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <template>
-  <router-view/>
-    <p>fdlkj</p>
+<div class="container">
+    <div class="menu">
+        <LeftBar/>
+    </div>
+    <div class="router_container">
+        <router-view/>
+    </div>
+</div>
 </template>
 
 <script lang="ts">
 
 import {Options, Vue} from "vue-class-component";
+import LeftBar from "@/components/UI/LeftBar.vue";
+import CoolButton from "@/components/UI/CoolButton.vue";
 
 @Options({
     name: 'App',
-    components: {},
+    components: {CoolButton, LeftBar},
 })
 export default class App extends Vue {
 
@@ -17,5 +25,24 @@ export default class App extends Vue {
 </script>
 
 <style scoped>
+.container {
+    display: grid;
+    grid-template-columns: min-content 1fr;
+}
 
+.router_container {
+    display: flex;
+    justify-content: center;
+    align-content: center;
+    align-items: center;
+}
+
+.menu {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    position: sticky;
+    top: 0;
+    height: 100vh;
+}
 </style>
