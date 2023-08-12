20d243fa6dab1b7d5af6ddd4494f02c818528a60
scobca <scobca18@yandex.ru>
2023-08-12 12:36:02 +0300

    layout for TableSelector.vue

---
+++ src/components/UI/TableSelector.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="container">
+        <div class="button_container">
+            <CoolButton class="button">План-факта выполнения операций</CoolButton>
+            <CoolButton class="button">План-факта выполнения операций</CoolButton>
+            <CoolButton class="button">Процент распределения сотрудников по операциям</CoolButton>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+
+import {Options, Vue} from "vue-class-component";
+import CoolButton from "@/components/UI/CoolButton.vue";
+
+@Options({
+    name: 'TableSelector',
+    components: {CoolButton},
+})
+export default class TableSelector extends Vue {
+    toggle: boolean | null = null
+}
+</script>
+
+<style scoped>
+.button_container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    align-self: center;
+    gap: 2rem;
+}
+
+.button {
+    width: 15rem;
+    height: 3.5rem;
+}
+</style>
